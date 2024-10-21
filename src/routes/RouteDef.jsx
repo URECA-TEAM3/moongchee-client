71be09ad22d8805a9cb575322d6a5ee51b2e734a
Jugwang <hjjk0721@gmail.com>
2024-10-21 14:43:53 +0900

    fix: router error fix

--- src/routes/RouteDef.jsx
+++ src/routes/RouteDef.jsx
@@ -1,6 +1,5 @@
 import Main from '../pages/main';
 import PetSitter from '../pages/petsitter';
-import Main from '../pages/Main';
 import AnimalInfo from '../pages/main/AnimalInfo';
 
 import LoginPage from '../pages/main/LoginPage';
@@ -27,6 +26,7 @@ const MainScreens = {
   Petsitter: {
     path: '/petsitter',
     element: <PetSitter />,
+  },
   AnimalInfo: {
     path: '/animalinfo',
     element: <AnimalInfo />,
