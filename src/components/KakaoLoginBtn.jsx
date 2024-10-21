2a8450edbcb9299d4edbafc6157d390903983fee
jin <dmlwls7094@naver.com>
2024-10-21 11:11:35 +0900

    카카오 소셜로그인 기능 완료

--- src/components/KakaoLoginBtn.jsx
+++ src/components/KakaoLoginBtn.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const KakaoLoginBtn = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (!window.Kakao.isInitialized()) {
       window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
@@ -18,7 +21,14 @@ const KakaoLoginBtn = () => {
             token: authObj.access_token,
           });
 
-          console.log('서버 응답:', response.data);
+          const { userId, exists } = response.data;
+          console.log('서버 응답으로 받은 유저 아이디:', userId);
+
+          if (exists) {
+            navigate('/main');
+          } else {
+            navigate('/signup', { state: { provider: 'kakao', userId } });
+          }
         } catch (error) {
           console.error('로그인 오류:', error);
         }
@@ -32,11 +42,11 @@ const KakaoLoginBtn = () => {
   return (
     <button onClick={handleKakaoLogin} style={{ border: 'none', background: 'transparent' }}>
       <img
-        src="/src/assets/images/kakaobtnimg.png"
+        src="/src/assets/images/kakaobtnimg.svg"
         alt="카카오 로그인"
         style={{
           cursor: 'pointer',
-          width: '200px',
+          width: '300px',
           height: '50px',
         }}
       />
