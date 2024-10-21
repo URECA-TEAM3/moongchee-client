1e5139c03c1b88811efd53df70e07d9a68c0c2fe
jin <dmlwls7094@naver.com>
2024-10-21 11:15:21 +0900

    로그인 페이지 구성

--- src/pages/main/LoginPage.jsx
+++ src/pages/main/LoginPage.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import KakaoLoginBtn from '../../components/KakaoLoginBtn';
 import GoogleLoginBtn from '../../components/GoogleLoginBtn';
-import '../../styles/LoginPage.css';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="login-container">
-      <div className="content">
-        <h1 className="title">
-          반려견의 행복수치 상승을 위한<br></br> 쇼핑과 펫시터 앱
+    <div className="flex justify-center items-center h-screen bg-[#2589e7]">
+      <div className="text-center text-white">
+        <h1 className="mt-12 text-sm">
+          반려견의 행복수치 상승을 위한
+          <br /> 쇼핑과 펫시터 앱
         </h1>
-        <img src="/src/assets/images/LoginLogo.png" alt="로고" className="logo" />
-        <div className="divider">SNS 계정으로 로그인</div>
-        <KakaoLoginBtn />
-        <GoogleLoginBtn />
+        <img src="/src/assets/images/LoginLogo.png" alt="로고" className="w-[322px] h-[365px] mx-auto my-6" />
+        <div className="flex items-center my-4">
+          <div className="flex-grow border-t border-white mx-2"></div>
+          <span className="text-white">SNS 계정으로 로그인</span>
+          <div className="flex-grow border-t border-white mx-2"></div>
+        </div>
+        <KakaoLoginBtn provider="kakao" />
+        <GoogleLoginBtn provider="google" />
       </div>
     </div>
   );
