2e7cd81ddc89d9d3ae00b0f47ee23328571a330f
skang9810 <suhjin9810@gmail.com>
2024-10-23 15:43:34 +0900

    fix: icon error fix

--- src/pages/main/AnimalRegisterSuccessPage.jsx
+++ src/pages/main/AnimalRegisterSuccessPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import successImage from '../../assets/images/successsignup.png';
+import successImage from '../../assets/images/white-curve.png';
 import { useNavigate } from 'react-router-dom';
 
 const AnimalRegisterSuccessPage = () => {
