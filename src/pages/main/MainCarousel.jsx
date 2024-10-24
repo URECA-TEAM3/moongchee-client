b941deafab8e37b46f83de77dba61e701c6cf802
skang9810 <suhjin9810@gmail.com>
2024-10-24 10:24:56 +0900

    feat: navbar color change, refactor: new dependency

--- src/pages/main/MainCarousel.jsx
+++ src/pages/main/MainCarousel.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/outline';
 
 const slides = [
   {
@@ -39,17 +40,13 @@ const ProductCarousel = () => {
           onClick={handlePrevClick}
           className="hover:bg-blue-900/20 text-white w-10 h-full text-center opacity-75 hover:opacity-100 z-10 p-0 m-0 duration-400"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-20 -ml-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-          </svg>
+          <ChevronLeftIcon className="h-8 w-20 -ml-5" stroke="currentColor" />
         </button>
         <button
           onClick={handleNextClick}
           className="hover:bg-blue-900/20 text-white w-10 h-full text-center opacity-75 hover:opacity-100 z-10 p-0 m-0 duration-400"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-20 -ml-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-          </svg>
+          <ChevronRightIcon className="h-8 w-20 -ml-5" stroke="currentColor" />
         </button>
       </div>
     </div>
--- src/pages/main/ProductCarousel.jsx
+++ src/pages/main/ProductCarousel.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ChevronRightIcon, ChevronLeftIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 const products = [
   {
@@ -51,9 +52,7 @@ const ProductCarousel = () => {
           className="absolute left-0 z-10 p-2 w-12 h-12 bg-divider/50 hover:bg-paleblue rounded-full"
           style={{ top: '50%', transform: 'translateY(-50%)' }}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-1" fill="none" viewBox="0 0 24 24" stroke="gray" strokeWidth={2}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-          </svg>
+          <ChevronLeftIcon className="h-6 w-6 ml-1" stroke="gray" />
         </button>
 
         <div className="overflow-hidden w-full h-full">
@@ -74,13 +73,7 @@ const ProductCarousel = () => {
                     href={product.href}
                     className="flex justify-center items-center border border-divider w-full rounded-lg mt-2 p-1 text-sm hover:bg-divider/50"
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-5 mr-1">
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                      />
-                    </svg>
+                    <ShoppingCartIcon stroke="currentColor" className="size-5 mr-1" />
                     담기
                   </a>
                   <div className="mt-2">
@@ -100,9 +93,7 @@ const ProductCarousel = () => {
           className="absolute right-0 z-10 p-2 w-12 h-12 bg-divider/50 hover:bg-paleblue rounded-full"
           style={{ top: '50%', transform: 'translateY(-50%)' }}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-1" fill="none" viewBox="0 0 24 24" stroke="gray" strokeWidth={2}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-          </svg>
+          <ChevronRightIcon className="h-6 w-6 ml-1" stroke="gray" />
         </button>
       </div>
     </div>
