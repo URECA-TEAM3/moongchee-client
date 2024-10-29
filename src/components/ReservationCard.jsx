3e2936185cef565ac4c161d06d60c00a103abb86
Jugwang <hjjk0721@gmail.com>
2024-10-30 00:57:46 +0900

    fext: reservation cancel

--- src/components/ReservationCard.jsx
+++ src/components/ReservationCard.jsx
@@ -1,38 +1,20 @@
 import React, { useState } from 'react';
-import Modal from '../components/Modal';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
-const ReservationCard = ({ info }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [status, setStatus] = useState('');
+const ReservationCard = ({ info, openModal }) => {
   const [isPetSitter, setIsPetSitter] = useState(true);
 
   const navigate = useNavigate();
 
-  const openModal = (value) => {
-    setIsModalOpen(true);
-    setStatus(value);
+  const openModalAction = (value) => {
+    openModal(value, info);
   };
-
-  const closeModal = () => setIsModalOpen(false);
 
   const handleStatusCode = () => {
     if (info.status === 'reserved') return '예약 완료';
     else if (info.status === 'cancelled') return '취소 완료';
     else if (info.status === 'completed') return '펫시팅 완료';
     else return '';
-  };
-
-  const handleReservationCancel = async () => {
-    try {
-      const res = await axios.post('http://localhost:3000/api/petsitter/reservation/cancel', { reservation_id: info.reservationId });
-      console.log('Cancellation response:', res.data);
-      alert('예약이 취소되었습니다.');
-    } catch (error) {
-      console.error('Error cancelling reservation:', error);
-      alert('예약 취소에 실패했습니다.');
-    }
   };
 
   const handleDetailClick = () => navigate('/petsitter/reservation/detail');
@@ -65,50 +47,21 @@ const ReservationCard = ({ info }) => {
       <div className="flex gap-5 mt-3">
         {!isPetSitter ? (
           <div className="flex gap-5 mt-3 w-full">
-            <button className="text-white bg-delete px-4 py-2 rounded-lg font-normal w-full" onClick={() => openModal('reject')}>
+            <button className="text-white bg-delete px-4 py-2 rounded-lg font-normal w-full" onClick={() => openModalAction('reject')}>
               거절
             </button>
-            <button className="text-white bg-primary px-4 py-2 rounded-lg font-normal w-full" onClick={() => openModal('confirm')}>
+            <button className="text-white bg-primary px-4 py-2 rounded-lg font-normal w-full" onClick={() => openModalAction('confirm')}>
               수락
             </button>
           </div>
         ) : info.status === 'reserved' ? (
-          <button className="text-white bg-primary px-4 py-2 rounded-lg font-normal w-full" onClick={() => openModal('cancel')}>
+          <button className="text-white bg-primary px-4 py-2 rounded-lg font-normal w-full" onClick={() => openModalAction('cancel')}>
             예약 취소하기
           </button>
         ) : (
           ''
         )}
       </div>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        title={
-          status === 'confirm' ? (
-            <div>
-              <span className="text-lg font-bold mr-2">{info.name}</span>님의 요청을 수락하시겠습니까?
-            </div>
-          ) : status === 'reject' ? (
-            <div>
-              <span className="text-lg font-bold mr-2">{info.name}</span>님의 요청을 거절하시겠습니까?
-            </div>
-          ) : (
-            <div>
-              <span className="text-lg font-bold mr-2">{info.name}</span>님에게 예약된 예약건을 취소하시겠습니까?
-            </div>
-          )
-        }
-      >
-        <div className="my-10 flex justify-center">
-          <span className="text-alert font-bold text-lg">* 승인을 하면 취소 할 수 없습니다.</span>
-        </div>
-        <div className="flex gap-4 mt-3">
-          <button className="text-white bg-divider px-4 py-2 rounded-lg font-normal w-full" onClick={closeModal}>
-            취소
-          </button>
-          <button className="text-white bg-primary px-4 py-2 rounded-lg font-normal w-full">수락</button>
-        </div>
-      </Modal>
     </div>
   );
 };
--- src/pages/petsitterReservationList/index.jsx
+++ src/pages/petsitterReservationList/index.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import ReservationCard from '../../components/ReservationCard';
+import Modal from '../../components/Modal';
 import axios from 'axios';
 
 const index = () => {
   const [reservationList, setReservationList] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [status, setStatus] = useState('');
+  const [selectedReservation, setSelectedReservation] = useState({
+    name: '',
+    reservationId: '',
+  });
 
   const handleReservationList = async () => {
     try {
       const res = await axios.post('http://localhost:3000/api/petsitter/reservation/list', {
-        user_id: 1,
-        user_type: 'user',
+        user_id: 1, // 나중에 user 에서 가져옴
+        user_type: 'user', // 마찬가지
       });
 
       const reservationList = res.data.data.map((item) => {
@@ -21,22 +28,36 @@ const index = () => {
           status: item.status,
           reservationId: item.reservation_id,
         };
-        // {
-        //   endTime: '12:00';
-        //   name: '박지훈';
-        //   requestDate: '2023-10-20';
-        //   reservation_id: 1;
-        //   sitter_id: 3;
-        //   sitter_profile_image: 'https://example.com/image3.jpg';
-        //   startTime: '10:00';
-        //   status: 'reserved';
-        //   user_id: 1;
-        // }
       });
 
       setReservationList(reservationList);
     } catch (error) {}
   };
+
+  const handleReservationCancel = async () => {
+    try {
+      const res = await axios.post('http://localhost:3000/api/petsitter/reservation/cancel', { reservation_id: selectedReservation.reservationId });
+      console.log('Cancellation response:', res.data);
+      alert('예약이 취소되었습니다.');
+      closeModal();
+      handleReservationList();
+    } catch (error) {
+      console.error('Error cancelling reservation:', error);
+      alert('예약 취소에 실패했습니다.');
+      closeModal();
+    }
+  };
+
+  const openModal = (value, info) => {
+    setIsModalOpen(true);
+    setSelectedReservation({
+      name: info.name,
+      reservationId: info.reservationId,
+    });
+    setStatus(value);
+  };
+
+  const closeModal = () => setIsModalOpen(false);
 
   useEffect(() => {
     handleReservationList();
@@ -45,8 +66,39 @@ const index = () => {
   return (
     <div className="px-10 flex flex-col gap-10">
       {reservationList.map((item, index) => (
-        <ReservationCard key={item.name + index} info={item} />
+        <ReservationCard key={item.name + index} info={item} openModal={openModal} />
       ))}
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title={
+          status === 'confirm' ? (
+            <div>
+              <span className="text-lg font-bold mr-2">{selectedReservation.name}</span>님의 요청을 수락하시겠습니까?
+            </div>
+          ) : status === 'reject' ? (
+            <div>
+              <span className="text-lg font-bold mr-2">{selectedReservation.name}</span>님의 요청을 거절하시겠습니까?
+            </div>
+          ) : (
+            <div>
+              <span className="text-lg font-bold mr-2">{selectedReservation.name}</span>님에게 예약된 예약건을 취소하시겠습니까?
+            </div>
+          )
+        }
+      >
+        <div className="my-10 flex justify-center">
+          <span className="text-alert font-bold text-lg">* 승인을 하면 취소 할 수 없습니다.</span>
+        </div>
+        <div className="flex gap-4 mt-3">
+          <button className="text-white bg-divider px-4 py-2 rounded-lg font-normal w-full" onClick={closeModal}>
+            취소
+          </button>
+          <button className="text-white bg-primary px-4 py-2 rounded-lg font-normal w-full" onClick={handleReservationCancel}>
+            수락
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
