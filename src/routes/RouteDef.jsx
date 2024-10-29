9d3d35a14adf03a957e0ffb0b27e7e33b16d7075
Euijin <162551578+Eu1j1n@users.noreply.github.com>
2024-10-29 13:23:23 +0900

    Feature/login02 (#10)
    * feat:프로필 이미지 기능을 위한 이미지 파일 추가
    
    * Feat: 프로필사진,펫사진 등록을 위한 이미지 추가
    
    * Feat: FireBase에 펫 프로필 사진 등록 기능 완료
    
    * Design: 로고 크기 수정
    
    * Feat: FireBase에 유저 프로필 사진 등록 기능 완료
    
    * path changed
    
    * path changed
    
    * path changed
    
    * path changed
    
    * Feat: 닉네임 중복확인기능 및 db 저장 완료
    
    * Feat: 애완동물등록성공페이지 라우터 설정
    
    * 파이어베이스 sdk 초기화
    
    * 의존성 추가
    
    * 의존성 추가
    
    * .env 추가
    
    * 스펠링 수정
    
    * 스펠링 수정
    
    * 카카오 로그인 인증 강화
    
    * 라우터 오류수정
    
    * 구글로그인 엑세스토큰 만료기간에따른 요청 기능 추가
    
    * 카카오 로그인 엑세스토큰 만료기간에 따른 요청 기능 추가
    
    * fix: 버그 수정
    
    * fix: 버그 수정
    
    * 라우터 경로 수정
    
    * 의존성 삭제
    
    * 폴더 수정
    
    * DogChew  이용하는 ChargePage로 변경
    
    * 소셜로그인 버그 수정
    
    * Fix: 뒤로가기 했을때, main으로 넘어가질수 있는 버그 수정
    
    * Fix: 뒤로가기 했을때, main으로 넘어가질수 있는 버그 수정
    
    * Fix: 뒤로가기 했을때, main으로 넘어가질수 있는 버그 수정 및 닉네임 정규화 수정
    
    * 해당 유저 데이터 세션스토리지에 저장
    
    * 해당 유저 데이터 세션스토리지에 저장
    
    * 서진 리뷰 반영
    
    * 엑세스 토큰이 만료안되었을때도, userdata 불러오기
    
    * 엑세스 토큰이 만료안되었을때도, userdata 불러오기
    
    * 회원가입 성공시 새션스토리지에 유저데이터 올리기
    
    * 닉네임 정규화식 변경

--- src/routes/RouteDef.jsx
+++ src/routes/RouteDef.jsx
@@ -20,6 +20,7 @@ import AnimalRegisterSuccessPage from '../pages/main/AnimalRegisterSuccessPage';
 import ShoppingCart from '../pages/shoppingcart/ShoppingCart';
 import Chat from '../pages/chat/Chat';
 import Mypage from '../pages/mypage/Mypage';
+import ChargePage from '../pages/mypage/ChargePage';
 
 const MainScreens = {
   LoginPage: {
@@ -57,6 +58,11 @@ const MainScreens = {
   Mypage: {
     path: '/mypage',
     element: <Mypage />,
+  },
+
+  ChargePage: {
+    path: '/chargepage',
+    element: <ChargePage />,
   },
 };
 
