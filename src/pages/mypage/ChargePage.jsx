1bdd8278fe769ea5de8bf7df9e00cd180693d110
Suhjin Kang <55722299+skang9810@users.noreply.github.com>
2024-10-31 16:01:31 +0900

    Feature / cancel order (#23)
    * get point by id
    
    * saving progress
    
    * added modal before payment
    
    * feat: refund point when canceled
    
    * fix: checkout.jsx 오타 수정
    
    ---------
    
    Co-authored-by: 김예원 <yaewon4643@naver.com>

--- src/pages/mypage/ChargePage.jsx
+++ src/pages/mypage/ChargePage.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import backButtonIcon from '/src/assets/icons/backbtn.svg';
 import { useNavigate } from 'react-router-dom';
 import DogChew from '../../components/DogChew';
+import axios from 'axios';
 
 const ChargePage = () => {
+  const userData = sessionStorage.getItem('userData');
+  const parsedData = userData ? JSON.parse(userData) : null;
+  const [id, setId] = useState(parsedData.id);
   const [selectedAmount, setSelectedAmount] = useState(0);
-  const [currentBones, setCurrentBones] = useState(100);
+  const [currentBones, setCurrentBones] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
-
+  const amounts = [1, 3, 5, 7, 10];
   const additionalBones = {
     1: 0,
     3: 10,
@@ -16,7 +21,23 @@ const ChargePage = () => {
     10: 100,
   };
 
-  const amounts = [1, 3, 5, 7, 10];
+  const getPoint = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/api/members/point/${id}`);
+
+      if (response.status !== 200) {
+        console.error('Error retrieving point', response);
+        return;
+      }
+      setCurrentBones(response.data.data.point);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPoint();
+  }, []);
 
   const getButtonClass = (amount) => {
     return selectedAmount === amount
@@ -77,13 +98,30 @@ const ChargePage = () => {
         </div>
 
         {selectedAmount ? (
-          <button onClick={() => navigate(`/checkout?price=${selectedAmount * 10000}`)} className="h-12 w-[70%] bg-primary text-white text-lg rounded-lg">
+          <button onClick={() => setIsModalOpen(true)} className="h-12 w-[70%] bg-primary text-white text-lg rounded-lg">
             결제하기
           </button>
         ) : (
           <button className="h-12 w-[70%] bg-gray-300 text-white text-lg rounded-lg">결제하기</button>
         )}
       </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg shadow-lg text-center w-80 h-auto p-6">
+            <h2 className="text-base font-extrabold mb-6">결제하시겠습니까?</h2>
+            <h3 className="text m-10 p-2">포인트 적립을 위한 결제는 환불이 불가능합니다.</h3>
+            <div className="flex justify-center space-x-4">
+              <button onClick={() => setIsModalOpen(false)} className="px-12 py-2 bg-divider text-gray-500 rounded-lg">
+                취소
+              </button>
+              <button onClick={() => navigate(`/checkout?price=${selectedAmount * 10000}`)} className="px-12 py-2 bg-delete text-white rounded-lg">
+                확인
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
