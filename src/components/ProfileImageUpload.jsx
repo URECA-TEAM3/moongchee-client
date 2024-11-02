d8d38384682684c8ea561451909c3721ddb03c6d
김의진 <ejkim@gim-uijin-ui-MacBookAir.local>
2024-11-02 16:23:36 +0900

    프로필이미지 업로드 컴포넌트화

---
+++ src/components/ProfileImageUpload.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import defaultProfileImage from '/src/assets/images/user.svg';
+
+const ProfileImageUpload = ({ selectedImage, setSelectedImage, setSelectedImageFile }) => {
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedImage(URL.createObjectURL(file));
+      setSelectedImageFile(file);
+    }
+  };
+
+  const handleProfileClick = () => {
+    document.getElementById('profileImageUpload').click();
+  };
+
+  return (
+    <div className="relative w-20 h-20 overflow-hidden cursor-pointer" onClick={handleProfileClick}>
+      {selectedImage ? (
+        <img src={selectedImage} alt="프로필 이미지" className="w-full h-full object-cover rounded-full" />
+      ) : (
+        <img src={defaultProfileImage} alt="기본 프로필 이미지" className="w-full h-full object-contain" />
+      )}
+      <input type="file" id="profileImageUpload" accept="image/*" className="hidden" onChange={handleImageChange} />
+    </div>
+  );
+};
+
+export default ProfileImageUpload;
