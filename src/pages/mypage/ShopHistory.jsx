d95c49cced29facd0d0a878e0f8b3edd0332a6cd
김예원 <yaewon4643@naver.com>
2024-11-03 14:19:41 +0900

    commit before fetch petsitter

--- src/pages/mypage/ShopHistory.jsx
+++ src/pages/mypage/ShopHistory.jsx
@@ -1,8 +1,8 @@
 import { ChevronDownIcon, ChevronLeftIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DogChew from '../../components/DogChew';
-import axios from 'axios';
+import API from '../../api/axiosInstance';
 import Modal from '../../components/Modal';
 
 const ShopHistory = () => {
@@ -13,21 +13,41 @@ const ShopHistory = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [cancelPrice, setCancelPrice] = useState(0);
+  const [orderHistory, setOrderHistory] = useState([]);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
 
-  // 구매 내역 데이터 (예시)
-  const items = [
-    { id: 1, name: '순우리 국산 개껌 (4개입)', quantity: 1, price: 50 },
-    { id: 2, name: '슈퍼포우 오메가3 120ml', quantity: 1, price: 50 },
-    { id: 3, name: '순우리 국산 개껌 (4개입)', quantity: 1, price: 50 },
-  ];
+  // Fetch order_item
+  useEffect(() => {
+    const OrderHistory = async () => {
+      try {
+        const response = await API.get(`/cart/order/${id}`);
+
+        // Grouping data by date
+        const groupedData = response.data.reduce((acc, order) => {
+          const date = order.order_date;
+          if (!acc[date]) {
+            acc[date] = [];
+          }
+          acc[date].push(order);
+          return acc;
+        }, {});
+        setOrderHistory(groupedData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (id) {
+      OrderHistory();
+    }
+  }, [id]);
 
   const refundPoint = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/api/members/update-points', {
+      const response = await API.post('members/update-points', {
         userId: id,
         amount: cancelPrice,
       });
@@ -59,72 +79,45 @@ const ShopHistory = () => {
       </div>
 
       <div className="pt-3 pb-7 pl-10 pr-10">
-        <div className="w-full bg-white rounded-lg p-5 shadow mb-5 flex justify-between items-center">
-          <div className="w-full flex flex-col justify-between">
-            <div className="flex w-full justify-between items-center">
-              <div className="flex items-center">
-                <span className="text-lg font-bold mb-1">2024.09.18</span>
-              </div>
-              <div>
-                <button onClick={toggleExpand}>
-                  {isExpanded ? (
-                    <div className="flex">
-                      <span className="mr-1">순우리 국산 개껌 (4개입)</span>
-                      <span className="text-primary mr-1"> 외 2개</span>
-                      <ChevronUpIcon className="h-6 w-6" stroke="black" />
-                    </div>
-                  ) : (
-                    <ChevronDownIcon className="h-6 w-6" stroke="black" />
-                  )}
-                </button>
-              </div>
+        {Object.keys(orderHistory).map((date) => (
+          <div key={date} className="w-full bg-white rounded-lg p-5 shadow mb-5">
+            <div className="flex justify-between items-center">
+              <div className="text-lg font-bold mb-1">{date}</div>
+              <button onClick={toggleExpand}>
+                {isExpanded ? (
+                  <ChevronUpIcon className="h-6 w-6" stroke="black" />
+                ) : (
+                  <ChevronDownIcon className="h-6 w-6" stroke="black" />
+                )}
+              </button>
             </div>
-            {!isExpanded && (
-              <div className="flex mt-2">
-                <img src="/src/assets/images/dog.jpeg" className="w-20 h-20 rounded-lg" />
-                <div className="p-4">
-                  <span>순우리 국산 개껌 (4개입)</span>
-                  <span className="text-primary"> 외 2개</span>
-                  <div className="p-1 flex">
-                    <DogChew />
-                    <p className="ml-2 font-bold">150개</p>
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {/* History detail */}
-            {isExpanded && (
-              <div>
-                {items.map((item) => (
-                  <div key={item.id} className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <img src="/src/assets/images/dog.jpeg" className="w-20 h-20 rounded-lg" />
-                      <div className="p-4">
-                        <p>{item.name}</p>
-                        <p>수량 : {item.quantity}개</p>
-                        <div className="flex">
-                          <DogChew />
-                          <p className="ml-2 font-bold">{item.price}개</p>
-                        </div>
+            {isExpanded &&
+              orderHistory[date].map((item) => (
+                <div key={item.id} className="flex items-center justify-between mt-4">
+                  <div className="flex items-center">
+                    <img src="/src/assets/images/dog.jpeg" className="w-20 h-20 rounded-lg" alt="Product" />
+                    <div className="p-4">
+                      <p>{item.name}</p>
+                      <p>수량 : {item.quantity}개</p>
+                      <div className="flex">
+                        <DogChew />
+                        <p className="ml-2 font-bold">{item.price}개</p>
                       </div>
                     </div>
-
-                    <button
-                      onClick={() => {
-                        setIsModalOpen(true);
-                        setCancelPrice(item.price);
-                      }}
-                      className="border border-primary text-primary text-sm rounded-lg w-16 h-7 hover:bg-primary hover:text-white"
-                    >
-                      취소
-                    </button>
                   </div>
-                ))}
-              </div>
-            )}
+                  <button
+                    onClick={() => {
+                      setIsModalOpen(true);
+                      setCancelPrice(item.price);
+                    }}
+                    className="border border-primary text-primary text-sm rounded-lg w-16 h-7 hover:bg-primary hover:text-white"
+                  >
+                    취소
+                  </button>
+                </div>
+              ))}
           </div>
-        </div>
+        ))}
       </div>
 
       <Modal isOpen={isModalOpen} onClose={closeModal} title={<div className="font-bold">주문을 취소하시겠습니까?</div>}>
