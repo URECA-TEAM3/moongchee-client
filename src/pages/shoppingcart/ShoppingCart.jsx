d00a6dae6d9958e0657847b8267428f711169f5e
김의진 <ejkim@gim-uijin-ui-MacBookAir.local>
2024-11-04 21:20:13 +0900

    장바구니 불러올때 Spinner 처리

--- src/pages/shoppingcart/ShoppingCart.jsx
+++ src/pages/shoppingcart/ShoppingCart.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import DogChew from '../../components/DogChew';
 import { Link, useNavigate } from 'react-router-dom';
-import { CiCircleMinus } from 'react-icons/ci';
-import { CiCirclePlus } from 'react-icons/ci';
+import { CiCircleMinus, CiCirclePlus } from 'react-icons/ci';
 import { AiOutlineMinus } from 'react-icons/ai';
 import { GoInfo } from 'react-icons/go';
 import PayInfo from '../../components/shop/PayInfo';
@@ -22,8 +21,10 @@ function ShoppingCart() {
   const [cartItems, setCartItems] = useState([]);
   const [showItems, setShowItems] = useState(true);
   const [points, setPoints] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getCartItemsList = async () => {
+    setIsLoading(true);
     try {
       const response = await API.get(`/cart/${id}`);
 
@@ -45,21 +46,23 @@ function ShoppingCart() {
       setCartItems(productsWithImages);
     } catch (error) {
       console.error();
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     const fetchPoints = async () => {
       try {
-        const userPoints = await getPoint(id); // getPoint 함수 호출
+        const userPoints = await getPoint(id);
         setPoints(userPoints);
       } catch (error) {
-        console.error('Error fetching points:', error); // 에러 처리
+        console.error('Error fetching points:', error);
       }
     };
 
     fetchPoints();
-    getCartItemsList(); // 장바구니 아이템 리스트 가져오기
+    getCartItemsList();
   }, [id]);
 
   // 최종 결제 금액 계산
@@ -166,102 +169,113 @@ function ShoppingCart() {
       <Toaster />
       <div className="text-center px-10 py-6 font-bold">장바구니</div>
 
-      {/* 장바구니 담긴 상품 조회 */}
-      <div className="mb-5">
-        <ul>
-          {cartItems.map((item) => (
-            <li key={item.cart_id} className="cart-item text-lg">
-              <div className="flex items-start w-full mx-auto py-5 px-10">
-                <input className="block mt-2 scale-125" type="checkbox" checked={item.checked} onChange={() => handleCheckboxChange(item.cart_id)} />
-                <div className="flex grow mr-2">
-                  <img src={item.image} alt={item.name} className="mx-7 cart-item-image w-[150px]" />
-                  <div className="flex flex-col">
-                    <span className="my-1">{item.name}</span>
-                    <span className="flex items-center mb-1">
-                      <DogChew />
-                      <div className="ml-2 font-bold">{item.price.toLocaleString()}개</div>
-                    </span>
-                    <div className="flex items-center">
-                      <button onClick={() => handleQuantityChange(item.cart_id, -1)}>
-                        <CiCircleMinus size={25} color={'#2589E7'} />
-                      </button>
-                      <span className="mx-2">{item.quantity}</span>
-                      <button onClick={() => handleQuantityChange(item.cart_id, 1)}>
-                        <CiCirclePlus size={25} color={'#2589E7'} />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <button onClick={() => handleDelete(item.cart_id)}>
-                  <IoMdClose className="mt-2" />
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {showItems ? (
-        <div className="grow flex flex-col justify-end">
-          <PayInfo totalPrice={totalPrice} />
-
-          <div className="flex justify-between items-start pt-3 px-10">
-            <div className="flex">
-              <span className="mr-2">나의 현재 </span> <DogChew />
-            </div>
-            {payment ? (
-              <div className="w-24 flex flex-col items-end gap-1">
-                <div>{points}개</div>
-                <div className="flex justify-between w-full pl-2">
-                  <AiOutlineMinus />
-                  <div>{totalPrice}개</div>
-                </div>
-                <div className="border border-text w-full" />
-              </div>
-            ) : (
-              <div className="flex items-center justify-between">
-                <div className="flex items-center text-[red] text-sm mr-2">
-                  잔액이 부족합니다.
-                  <GoInfo className="ml-1" />
-                </div>
-                <div>{points - totalPrice}개</div>
-              </div>
-            )}
-          </div>
-
-          {payment && (
-            <div className="flex justify-between items-center pb-3 mt-[0.25rem] px-10">
-              <div className="flex">
-                <span className="mr-2">결제 후 </span> <DogChew />
-              </div>
-              <div className="font-bold">{afterPayment}개</div>
-            </div>
-          )}
-
-          {payment ? (
-            <div className="text-center my-10">
-              <button onClick={handleCheckout} className="w-1/2 h-12 bg-primary text-white rounded-lg text-center">
-                결제하기
-              </button>
-            </div>
-          ) : (
-            <div className="flex flex-col items-center justify-center m-10">
-              <Link to="/chargepage" className="text-sm underline underline-offset-2">
-                지금 바로 충전하기
-              </Link>
-              <Link to="#" className="grow w-1/2 mt-2">
-                <div className="mx-auto bg-[#acacac] text-white p-3 mx-2 rounded-lg text-center">결제하기</div>
-              </Link>
-            </div>
-          )}
+      {isLoading ? (
+        <div className="flex justify-center items-center h-60">
+          <div className="w-12 h-12 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
         </div>
       ) : (
-        <div className="mb-20 grow flex flex-col items-center justify-center text-xl font-bold">
-          <img className="w-1/2 " src="/src/assets/images/black-curve.png" alt="" />
-          <div className="py-8">장바구니가 비었습니다.</div>
-          <button onClick={() => navigate('/shoppingmall')} className="w-6/12 mx-auto bg-primary text-white p-3 mx-2  rounded-lg text-center">
-            상품 구경하러가기
-          </button>
+        // 장바구니 내용
+        <div>
+          {/* 장바구니 담긴 상품 조회 */}
+          <div className="mb-5">
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.cart_id} className="cart-item text-lg">
+                  <div className="flex items-start w-full mx-auto py-5 px-10">
+                    <input className="block mt-2 scale-125" type="checkbox" checked={item.checked} onChange={() => handleCheckboxChange(item.cart_id)} />
+                    <div className="flex grow mr-2">
+                      <img src={item.image} alt={item.name} className="mx-7 cart-item-image w-[150px]" />
+                      <div className="flex flex-col">
+                        <span className="my-1">{item.name}</span>
+                        <span className="flex items-center mb-1">
+                          <DogChew />
+                          <div className="ml-2 font-bold">{item.price.toLocaleString()}개</div>
+                        </span>
+                        <div className="flex items-center">
+                          <button onClick={() => handleQuantityChange(item.cart_id, -1)}>
+                            <CiCircleMinus size={25} color={'#2589E7'} />
+                          </button>
+                          <span className="mx-2">{item.quantity}</span>
+                          <button onClick={() => handleQuantityChange(item.cart_id, 1)}>
+                            <CiCirclePlus size={25} color={'#2589E7'} />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <button onClick={() => handleDelete(item.cart_id)}>
+                      <IoMdClose className="mt-2" />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* 결제 섹션 */}
+          {showItems && !isLoading ? (
+            <div className="grow flex flex-col justify-end">
+              <PayInfo totalPrice={totalPrice} />
+
+              {/* 결제 금액 및 포인트 확인 */}
+              <div className="flex justify-between items-start pt-3 px-10">
+                <div className="flex">
+                  <span className="mr-2">나의 현재 </span> <DogChew />
+                </div>
+                {payment ? (
+                  <div className="w-24 flex flex-col items-end gap-1">
+                    <div>{points}개</div>
+                    <div className="flex justify-between w-full pl-2">
+                      <AiOutlineMinus />
+                      <div>{totalPrice}개</div>
+                    </div>
+                    <div className="border border-text w-full" />
+                  </div>
+                ) : (
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center text-[red] text-sm mr-2">
+                      잔액이 부족합니다.
+                      <GoInfo className="ml-1" />
+                    </div>
+                    <div>{points - totalPrice}개</div>
+                  </div>
+                )}
+              </div>
+
+              {payment && (
+                <div className="flex justify-between items-center pb-3 mt-[0.25rem] px-10">
+                  <div className="flex">
+                    <span className="mr-2">결제 후 </span> <DogChew />
+                  </div>
+                  <div className="font-bold">{afterPayment}개</div>
+                </div>
+              )}
+
+              {payment ? (
+                <div className="text-center my-10">
+                  <button onClick={handleCheckout} className="w-1/2 h-12 bg-primary text-white rounded-lg text-center">
+                    결제하기
+                  </button>
+                </div>
+              ) : (
+                <div className="flex flex-col items-center justify-center m-10">
+                  <Link to="/chargepage" className="text-sm underline underline-offset-2">
+                    지금 바로 충전하기
+                  </Link>
+                  <Link to="#" className="grow w-1/2 mt-2">
+                    <div className="mx-auto bg-[#acacac] text-white p-3 mx-2 rounded-lg text-center">결제하기</div>
+                  </Link>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="mb-20 grow flex flex-col items-center justify-center text-xl font-bold">
+              <img className="w-1/2" src="/src/assets/images/black-curve.png" alt="" />
+              <div className="py-8">장바구니가 비었습니다.</div>
+              <button onClick={() => navigate('/shoppingmall')} className="w-6/12 mx-auto bg-primary text-white p-3 mx-2  rounded-lg text-center">
+                상품 구경하러가기
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
