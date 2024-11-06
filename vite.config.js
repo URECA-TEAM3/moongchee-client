66436e31f3e6f4bca7ba8f5d50e8a8013be104d8
Euijin <162551578+Eu1j1n@users.noreply.github.com>
2024-11-06 09:28:09 +0900

    배포 전 콘솔 제거 (#56)
    Co-authored-by: 김의진 <ejkim@gim-uijin-ui-MacBookAir.local>

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
+import { babel } from '@rollup/plugin-babel'; // <--이것도
 
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [
+    react(),
+    eslintPlugin(),
+    babel({
+      // 콘솔 보고 싶으면 윗줄부터
+
+      babelHelpers: 'bundled',
+      plugins: ['babel-plugin-transform-remove-console'],
+      exclude: 'node_modules/**',
+
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }), // 여기까지 주석 치셈 !!
+  ],
   server: {
-    hmr: true, // HMR 활성화 여부
+    hmr: true,
     watch: {
       usePolling: true,
     },
