0c5f2ec78669db2e3b74033a159b73be469f7596
Jugwang <hjjk0721@gmail.com>
2024-11-08 11:12:48 +0900

    fix: vite config change

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { refreshAccessToken } from './login';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: '/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
